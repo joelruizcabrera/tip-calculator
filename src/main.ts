554db231100220ad9ead11b9fd6a32a99bbd7756
Joel Schwegmann <schwegmann@hob-by-horse.de>
2025-04-25 15:37:09 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,33 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import StartingView from "@/views/StartingView.vue";
+import CreateView from "@/views/CreateView.vue";
+
+import {createMemoryHistory, createRouter} from "vue-router";
+
 import App from './App.vue'
+
+const routes = [
+    {
+        path: '/',
+        component: StartingView
+    },
+    {
+        path: '/create',
+        component: CreateView
+    }
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(router)
 
 app.mount('#app')
